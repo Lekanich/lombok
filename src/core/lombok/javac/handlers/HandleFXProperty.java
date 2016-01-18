4771caea53516b53ed5be667fe7d1b791609e495
Tall Tamias <tall.tamias@gmail.com>
2016-01-18 16:24:45 +0200

    getter added

--- src/core/lombok/javac/handlers/HandleFXProperty.java
+++ src/core/lombok/javac/handlers/HandleFXProperty.java
@@ -1,13 +1,12 @@
 package lombok.javac.handlers;
 
-import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.type.ExecutableType;
+import java.util.HashMap;
+import java.util.Map;
 import com.sun.tools.javac.code.Flags;
 import com.sun.tools.javac.code.Symbol;
-import com.sun.tools.javac.code.Symbol.VarSymbol;
 import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.code.Type.ClassType;
-import com.sun.tools.javac.model.JavacTypes;
+import com.sun.tools.javac.code.Type.TypeVar;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.JCTree.JCAnnotation;
 import com.sun.tools.javac.tree.JCTree.JCBlock;
@@ -17,6 +16,7 @@ import com.sun.tools.javac.tree.JCTree.JCStatement;
 import com.sun.tools.javac.tree.JCTree.JCTypeParameter;
 import com.sun.tools.javac.tree.JCTree.JCVariableDecl;
 import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.ListBuffer;
 import com.sun.tools.javac.util.Name;
 import lombok.AccessLevel;
 import lombok.core.AST.Kind;
@@ -106,20 +106,21 @@ public class HandleFXProperty extends JavacAnnotationHandler<FXProperty> {
 					return true;
 			}
 		}
+
 		return false;
 	}
 
 	public JCMethodDecl createGetter(AccessLevel level, JavacNode field, JavacTreeMaker treeMaker, JavacNode annotationNode) {
 		JCVariableDecl fieldNode = (JCVariableDecl) field.get();
+		String delegatingMethodName = "getValue";
 
 		Name methodName = field.toName(toGetterName(field));
-		JCExpression methodType = findType(treeMaker, fieldNode, annotationNode.getTypesUtil());
-		List<JCStatement> statements = createGetterBody(treeMaker, field);
+		JCExpression methodType = findType(treeMaker, fieldNode, delegatingMethodName);
+		List<JCStatement> statements = createMethodBody(treeMaker, field, delegatingMethodName);
 		List<JCTypeParameter> methodGenericParams = List.nil();
 		List<JCVariableDecl> parameters = List.nil();
 		List<JCExpression> throwsClauses = List.nil();
 		JCBlock methodBody = treeMaker.Block(0, statements);
-		JCExpression annotationMethodDefaultValue = null;
 
 		JCMethodDecl decl = treeMaker.MethodDef(treeMaker.Modifiers(JavacHandlerUtil.toJavacModifier(level)),
 									methodName,
@@ -128,47 +129,93 @@ public class HandleFXProperty extends JavacAnnotationHandler<FXProperty> {
 									parameters,
 									throwsClauses,
 									methodBody,
-									annotationMethodDefaultValue);
+									null);
 
-//		copyJavadoc(field, decl, CopyJavadoc.GETTER);
 		return decl;
 	}
 
-	public List<JCStatement> createGetterBody(JavacTreeMaker treeMaker, JavacNode field) {
+	public List<JCStatement> createMethodBody(JavacTreeMaker treeMaker, JavacNode field, String methodName) {
 //		boolean lookForGetter = lookForGetter(field, FieldAccess.ALWAYS_FIELD);
 
 		JCExpression expression = treeMaker.Apply(List.<JCExpression>nil(),
-				JavacHandlerUtil.chainDots(field, field.getName(), "getValue"), List.<JCExpression>nil());
+				JavacHandlerUtil.chainDots(field, field.getName(), methodName), List.<JCExpression>nil());
 		return List.<JCStatement>of(treeMaker.Return(expression));
 	}
 
-	public JCExpression findType(JavacTreeMaker treeMaker, JCVariableDecl fieldNode, JavacTypes types) {
-		Type resType = null;
+	public JCExpression findType(JavacTreeMaker treeMaker, JCVariableDecl fieldNode, String methodName) {
+		Type resType;
+		TypeParameterMap map = new TypeParameterMap();
 
-		ClassType superType = (ClassType) fieldNode.sym.type;
-		if (superType.supertype_field == null) {
-			superType = (ClassType) ((ClassType) superType.tsym.type).supertype_field;
+		ClassType currentType = (ClassType) fieldNode.sym.type;
+		if (currentType.isParameterized()) {
+			map.mapParameters(currentType);
+			currentType = takeSuperType(currentType);
 		} else {
-			superType = (ClassType) superType.supertype_field;
+			currentType = (ClassType) currentType.supertype_field;
 		}
 
-		while (superType != null && resType == null) {
-			for (Symbol sym : superType.tsym.getEnclosedElements()) {
-//				ExecutableElement element = (ExecutableElement) sym;
-				if (sym.getSimpleName().toString().equals("getValue")) {
-//					if (superType.isParameterized()) {
-//						resType = ((ClassType) fieldNode.vartype.type).typarams_field.head;
-//					} else {
-						ExecutableType type = (ExecutableType) types.asMemberOf(superType, sym);
-						resType = sym.asType().asMethodType().restype;
-//					}
-					break;
+		while (currentType != null) {
+			for (Symbol sym : currentType.tsym.getEnclosedElements()) {
+				if (sym.getSimpleName().toString().equals(methodName)) {
+					resType = sym.asType().asMethodType().restype;
+
+					if (resType instanceof TypeVar) {
+						resType = map.getParameterType(resType);
+					}
+
+					if (resType.isParameterized()) {
+						List<Type> parameters = map.getParametersType(((ClassType) resType.tsym.type).typarams_field);
+						ClassType type = new ClassType(resType, parameters, resType.tsym);
+						return treeMaker.Type(type);
+					}
+
+					return treeMaker.Type(resType);
 				}
 			}
 
-			superType = (ClassType)((ClassType) superType.tsym.type).supertype_field;
+			currentType = takeSuperType(currentType);
+			if (currentType.isParameterized()) {
+				map.mapParameters(currentType);
+			}
 		}
 
-		return treeMaker.Type(resType);
+		return null;
+	}
+
+	private ClassType takeSuperType(ClassType type) {
+		return (ClassType)((ClassType) type.tsym.type).supertype_field;
+	}
+
+	final private static class TypeParameterMap {
+		private Map<Name, Type> parameterMap = new HashMap<Name, Type>();
+
+		public void mapParameters(ClassType type) {
+			List<Type> parameters = type.typarams_field;
+			List<Type> parametersVar = ((ClassType) type.tsym.type).typarams_field;
+
+			if (parameters.length() == parametersVar.length()) {
+				for (int i = 0; i < parameters.length(); i++) {
+					Name parameterName = parametersVar.get(i).tsym.getSimpleName();
+					if (!parameterMap.containsKey(parameterName)) {
+						parameterMap.put(parameterName, parameters.get(i));
+					}
+				}
+			}
+		}
+
+		public List<Type> getParametersType(List<Type> types) {
+			ListBuffer<Type> list = new ListBuffer<Type>();
+			for (Type type : types) {
+				Name key = type.tsym.getSimpleName();
+				if (parameterMap.containsKey(key)) {
+					list.add(parameterMap.get(key));
+				}
+			}
+			return list.toList();
+		}
+
+		public Type getParameterType(Type type) {
+			return parameterMap.get(type.tsym.getSimpleName());
+		}
 	}
 }
