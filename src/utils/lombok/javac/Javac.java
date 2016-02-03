0c6da7a0e0b13a19c8485514a4006a359222504b
Tall Tamias <tall.tamias@gmail.com>
2016-02-03 10:56:11 +0200

    refactor

--- src/utils/lombok/javac/Javac.java
+++ src/utils/lombok/javac/Javac.java
@@ -24,17 +24,14 @@ package lombok.javac;
 import static lombok.javac.JavacTreeMaker.TreeTag.treeTag;
 import static lombok.javac.JavacTreeMaker.TypeTag.typeTag;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.type.NoType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeVisitor;
